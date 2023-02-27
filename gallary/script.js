7a6405fd979fbd29ba87a240c5c2d851f6cf9bff
AmineHeroual <aminehero@protonmail.com>
2023-02-28 00:23:23 +0100

    add new project portfolio

---
+++ gallary/script.js
@@ -0,0 +1,36 @@
+const gallery = document.querySelector(".container");
+
+window.onmousemove = (e) => {
+  const mouseX = e.clientX,
+    mouseY = e.clientY;
+
+  const xDecimal = mouseX / window.innerWidth,
+    yDecimal = mouseY / window.innerHeight;
+
+  const maxX = gallery.offsetWidth - window.innerWidth,
+    maxY = gallery.offsetHeight - window.innerHeight;
+
+  const panX = maxX * xDecimal * -1,
+    panY = maxY * yDecimal * -1;
+
+  gallery.animate(
+    {
+      transform: `translate(${panX}px, ${panY}px)`,
+    },
+    {
+      duration: 4000,
+      fill: "forwards",
+      easing: "ease",
+    }
+  );
+};
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = (e) => {
+  const mx = e.clientX,
+    my = e.clientY;
+
+  blob.style.left = `${mx}px`;
+  blob.style.top = `${my}px`;
+};
